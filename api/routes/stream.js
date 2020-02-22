8383e0e6bb29851175c55e1d48cba2519de3219c
Natnael Abrham <natnael08@protonmain.com>
2020-02-22 14:09:58 +0300

    create new stream route

--- api/routes/stream.js
+++ api/routes/stream.js
@@ -1,9 +1,14 @@
 // RADIO STREAM ROUTE //
 
 const router = require("express").Router();
+const protectedRoute = require("../middleware/protected-route");
 
-router.use("/", (req, res, next) => {
-  res.send("streaming route");
-});
+// controllers
+const createNew = require("../controllers/stream/createNew");
+
+// @route       POST stream/new
+// @desc        create new radio stream
+// @access      private
+router.post("/new", protectedRoute, createNew);
 
 module.exports = router;
