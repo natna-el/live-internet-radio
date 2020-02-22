9fc07ea21844618a3c35ada6e0d572e951074186
Natnael Abrham <natnael08@protonmain.com>
2020-02-22 12:10:12 +0300

    userType added

--- api/models/user.js
+++ api/models/user.js
@@ -9,6 +9,7 @@ const UserSchema = new Schema({
   username: { type: String, set: toLower, required: true, unique: true },
   password: { type: String, required: true },
   pfp: { type: String },
+  userType: { type: Number, default: 0 },
   Date: { type: Date, default: Date.now }
 });
 
